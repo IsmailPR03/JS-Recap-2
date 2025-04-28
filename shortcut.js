cb7738bb17fe9962b43f8bda34c21c2a9c2e40cf
rockys1 <rocky.sj26@gmail.com>
2025-04-29 02:12:58 +0600

    shortcut

---
+++ shortcut.js
@@ -0,0 +1,22 @@
+/**
+ * LOGICAL NOT OPERATOR ( ! )
+ * 
+ * LOGICAL NOT is also used with non-Boolean values
+ */
+
+const isLeader = true;
+// if (isLeader === true) likha j kotha " if (isLeader) " likhao eki kotha..eta hocche true check korar shortcut system
+// if (isLeader) {
+//     console.log('Leader asche! Chair charo')
+// } else {
+//     console.log('Vitore dukte dibi na')
+// }
+
+const isPassed = false;
+// if (isPassed === false) likha j kotha " if (!isPassed) " likhao eki kotha..eta hocche false check korar shortcut system
+if (!isPassed) {
+    console.log('Toke biye diye dimu');
+} 
+else {
+    console.log('Tumi amader noyon er moni')  
+}
