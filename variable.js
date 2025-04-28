6761d006dde05f82f5627ea22c8608003e286df9
rockys1 <rocky.sj26@gmail.com>
2025-04-29 02:07:57 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,33 @@
+/**
+ * variable ==> container
+ */
+
+// Number
+var sunglassPrice = 70;
+
+// String
+var subject = 'EEE';
+
+// Boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword/reserve word
+ * 6. prefer camelCase
+ */
+
+var lastExamResult = 89;
+
+/**
+ * OPERATION
+ * 1. +, -, *. /, %
+ * 2. +=, -=, *=. /=, %=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */
