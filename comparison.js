b6c7dc84de004a105822a42264744e5f68f9ca3e
rockys1 <rocky.sj26@gmail.com>
2025-04-29 02:08:52 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,23 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 <= 10);
+
+
+/**
+ * ==  // jokhon "==" use kori tokhon shudhu man ta k tulona kore
+ * ===  // jokhon "===" use kori she tokhon value + type tao check kore.
+ * 
+*/
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log(120 === 'Alu');
+
+// -----------------------------------
+console.log(10 != '10');
+console.log(10 !== '10'); // ekhane '!==' diye osoman kina ta check korlam + type check kora hoice
+
