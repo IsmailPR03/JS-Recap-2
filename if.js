1891c8426c5d62bc49a762934bc790d838fe04db
rockys1 <rocky.sj26@gmail.com>
2025-04-29 02:09:43 +0600

    if condition

---
+++ if.js
@@ -0,0 +1,46 @@
+/**
+ * tui jodi amake khawas
+ *  taile ami jabo
+ * 
+ * ----------------------
+ * if you buy food for me
+ *     i will come
+ * 
+ * ----------------------
+ * if biriyani price less than 300 taka
+ *     i will buy
+ * 
+ * 
+ * -----------STRUCTURE-----------
+ * if(condition){
+ * 
+ * }
+ */
+
+if (13 < 10) {
+    console.log('I have a smaller value');
+}
+
+var biriyaniPrice = 250;
+
+if (biriyaniPrice < 300) {
+    console.log('Mama give me some biriyani');
+}
+
+/**
+ * if(condition){
+ *      execute code if condition above is true
+ * }
+ * else{
+ *      execute code if condition is false
+ * }
+*/ 
+
+var weight = 20;
+
+if (weight < 20) {
+    console.log('I will carry it by myself');
+}
+else {
+    console.log('I will rent a rickshaw');
+}
